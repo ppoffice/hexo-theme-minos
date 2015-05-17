c461ce40180dfaa83fbbd0ef483eb0bbc713b928
ppoffice <ppoffice_2008@163.com>
2015-05-17 16:31:57 +0800

    first blood

---
+++ source/js/script.js
@@ -0,0 +1,28 @@
+(function($){
+
+  // Caption
+  $('.article-entry').each(function(i){
+    $(this).find('img').each(function(){
+      if ($(this).parent().hasClass('fancybox')) return;
+
+      var alt = this.alt;
+
+      if (alt) $(this).after('<span class="caption">' + alt + '</span>');
+
+      $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>');
+    });
+
+    $(this).find('.fancybox').each(function(){
+      $(this).attr('rel', 'article' + i);
+    });
+  });
+
+  if ($.fancybox){
+    $('.fancybox').fancybox();
+  }
+
+  // Mobile nav
+  $('#main-nav-toggle').click(function () {
+    $('#header').toggleClass('mobile-on');
+  });
+})(jQuery);
