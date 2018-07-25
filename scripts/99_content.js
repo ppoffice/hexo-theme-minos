edb018a1b4b26a68e0a171d891e957018ab61797
ppoffice <zrp1994@gmail.com>
2018-07-25 12:53:15 -0500

    fix(style): two code highlight issues
    - code does not highlight in post excerpt
    - keywords inside other keywords do not highlight

--- scripts/99_content.js
+++ scripts/99_content.js
@@ -163,18 +163,23 @@ hexo.extend.helper.register('toc_list', (content) => {
     return tocList;
 });
 
-/**
- * Add .hljs class name to the code blocks and code elements
- */
-hexo.extend.filter.register('after_post_render', function (data) {
-    const $ = cheerio.load(data.content);
+function patchCodeHighlight(content) {
+    const $ = cheerio.load(content);
     $('figure.highlight').addClass('hljs');
-    $('figure.highlight .code .line > span').each(function () {
+    $('figure.highlight .code .line span').each(function () {
         const classes = $(this).attr('class').split(' ');
         if (classes.length === 1) {
             $(this).addClass('hljs-' + classes[0]);
         }
     });
-    data.content = $.html();
+    return $.html();
+}
+
+/**
+ * Add .hljs class name to the code blocks and code elements
+ */
+hexo.extend.filter.register('after_post_render', function (data) {
+    data.content = data.content ? patchCodeHighlight(data.content) : data.content;
+    data.excerpt = data.excerpt ? patchCodeHighlight(data.excerpt) : data.excerpt;
     return data;
 });
